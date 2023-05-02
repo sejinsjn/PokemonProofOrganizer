6e01c6d2b0c143c9f8d5c83ef77c3db45b654a8b
sedad <sedad@BARUSU>
2023-05-02 20:58:10 +0200

    Add project files.

---
+++ PokemonProofOrganizer/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PokemonProofOrganizer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static bool renameChecked = false;
+        private static bool createFolderChecked = false;
+        private static bool addTradeHistoryChecked = false;
+        private static bool compressChecked = false;
+        private static string[] filePaths = null;
+        private static int ternary = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseFiles(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "Document"; // Default file name
+            dlg.DefaultExt = ".mp4"; // Default file extension
+            dlg.Filter = "Videos |*.mp4"; // Filter files by extension
+            dlg.Multiselect = true; //Select multiple files
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                filePaths = dlg.FileNames;
+            }
+        }
+
+        private void RenameChecked(object sender, RoutedEventArgs e)
+        {
+            renameChecked = true;
+        }
+
+        private void RenameUnchecked(object sender, RoutedEventArgs e)
+        {
+            renameChecked = false;
+        }
+
+        private void CreateFolderChecked(object sender, RoutedEventArgs e)
+        {
+            createFolderChecked = true;
+        }
+
+        private void CreateFolderUnchecked(object sender, RoutedEventArgs e)
+        {
+            createFolderChecked = false;
+        }
+
+        private void AddTradeHistoryChecked(object sender, RoutedEventArgs e)
+        {
+            addTradeHistoryChecked = true;
+        }
+
+        private void AddTradeHistoryUnchecked(object sender, RoutedEventArgs e)
+        {
+            addTradeHistoryChecked = false;
+        }
+
+        private void CompressChecked(object sender, RoutedEventArgs e)
+        {
+            compressChecked = true;
+        }
+
+        private void CompressUnchecked(object sender, RoutedEventArgs e)
+        {
+            compressChecked = false;
+        }
+
+        private void PathAdded(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void TernaryNumberChanged(object sender, TextChangedEventArgs e)
+        {
+            ternary = int.Parse(TernaryNumber.Text);
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            Tools tools = new Tools(renameChecked, createFolderChecked, addTradeHistoryChecked, compressChecked);
+            tools.runTools(filePaths, ternary);
+        }
+    }
+}
---
+++ PokemonProofOrganizer/Tools.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+
+namespace PokemonProofOrganizer
+{
+    internal class Tools
+    {
+        private static bool rename = false;
+        private static bool createFolder = false;
+        private static bool addTradeHistory = false;
+        private static bool compress = false;
+
+        public Tools(bool renameChecked, bool createFolderChecked, bool addTradeHistoryChecked, bool compressChecked)
+        {
+            rename = renameChecked;
+            createFolder = createFolderChecked;
+            addTradeHistory = addTradeHistoryChecked;
+            compress = compressChecked;
+        }
+
+        public void runTools(string[] filePaths, int ternary)
+        {
+            int currentNumber = ternary;
+            string fileName = "";
+
+            foreach (string filePath in filePaths)
+            {
+                fileName = "" + (12 * 10000 + DecimalToTernary(ternary));
+
+                if (rename)
+                {
+                    renameFiles(filePath, fileName + ".mp4");
+                }
+                
+
+                ternary++;
+            }
+        }
+
+        private void renameFiles(string filePath, string newFileName)
+        {
+            string directoryName = Path.GetDirectoryName(filePath);
+
+            // Construct the new file path by combining the directory and the new file name
+            string newFilePath = Path.Combine(directoryName, newFileName);
+
+            // Rename the file
+            File.Move(filePath, newFilePath);
+        }
+
+        private static int DecimalToTernary(int decimalNumber)
+        {
+            string ternaryNumber = "";
+
+            while (decimalNumber > 0)
+            {
+                int remainder = decimalNumber % 3;
+                ternaryNumber = remainder.ToString() + ternaryNumber;
+                decimalNumber /= 3;
+            }
+
+            if (ternaryNumber == "")
+            {
+                ternaryNumber = "0";
+            }
+
+            return int.Parse(ternaryNumber); ;
+        }
+
+    }
+}
